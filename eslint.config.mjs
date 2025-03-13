9dc89433c11871d684c8ef35d1cda98fdab75645
Abdullah Altun <a.altun70@outlook.com>
2025-03-13 15:03:14 +0100

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,57 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.mjs
+
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {FlatCompat} from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends(
+        'next/core-web-vitals',
+        'next/typescript',
+        'plugin:@tanstack/query/recommended',
+    ),
+    {
+        ignores: ['node_modules', '.*', 'public', 'coverage', 'out'],
+        rules: {
+            '@typescript-eslint/no-empty-object-type': 'off',
+            '@typescript-eslint/no-require-imports': 'off',
+            'import/order': [
+                'error',
+                {
+                    groups: [
+                        ['external', 'builtin'],
+                        'internal',
+                        ['sibling', 'parent'],
+                        'index',
+                    ],
+                    pathGroups: [
+                        {
+                            pattern: 'react',
+                            group: 'external',
+                            position: 'before',
+                        },
+                        {
+                            pattern: '@src/**',
+                            group: 'internal',
+                        },
+                    ],
+                    pathGroupsExcludedImportTypes: ['internal', 'react'],
+                    'newlines-between': 'always',
+                    alphabetize: {
+                        order: 'asc',
+                        caseInsensitive: true,
+                    },
+                },
+            ],
+        },
+    },
 ];
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    typescript: {
+        // Ignore type check during build, already performed as part of the CI/CD pipeline
+        ignoreBuildErrors: true,
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'src/styles')],
+    },
 };
 
 export default nextConfig;
